264f354b728301e4b9eb90b3f7f20a901e1f2e0e
Arsen Baktiyarov <baktiyarov21@gmail.com>
2021-08-26 09:49:47 +0600

    List all Owners on Owners List Page. Closes #9

--- pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/bootstrap/DataLoader.java
@@ -24,10 +24,22 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Owner owner = new Owner();
         owner.setId(1L);
-        owner.setFirstName("A");
-        owner.setLastName("B");
+        owner.setFirstName("Tony");
+        owner.setLastName("Ferguson");
         ownerService.save(owner);
-        System.out.println("Owner Created");
-        System.out.println(ownerService.findById(1L));
+        System.out.println("Owners loading....");
+        Owner owner1 = new Owner();
+        owner1.setId(2L);
+        owner1.setFirstName("Colby");
+        owner1.setLastName("Covington");
+        ownerService.save(owner1);
+        Owner owner2 = new Owner();
+        owner2.setId(3L);
+        owner2.setFirstName("Robert");
+        owner2.setLastName("Whittaker");
+        ownerService.save(owner2);
+        System.out.println("Owners was created");
+
+//        System.out.println(ownerService.findById(1L));
     }
 }
--- pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/controller/OwnerController.java
@@ -1,15 +1,28 @@
 package com.arsenbaktiyarov.sfgpetclinic.controller;
 
+import com.arsenbaktiyarov.sfgpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @GetMapping({"","/", "/list",
             "/owners-list","/owners-list.html"})
-    public String listOfOwners() {
+    public String listOfOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/owners-list";
     }
+
+
 }
