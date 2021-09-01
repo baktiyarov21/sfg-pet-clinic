d67da2f516a2e2b871781d9c05ce76f8731f73de
Arsen Baktiyarov <baktiyarov21@gmail.com>
2021-09-01 15:15:18 +0600

    adding visits, updated owner with pet relationship. Closes #18

---
+++ pet-clinic-data/src/main/java/com/arsenbaktiyarov/sfgpetclinic/model/Visit.java
@@ -0,0 +1,14 @@
+package com.arsenbaktiyarov.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Visit extends BaseEntity{
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+
+
+
+}
