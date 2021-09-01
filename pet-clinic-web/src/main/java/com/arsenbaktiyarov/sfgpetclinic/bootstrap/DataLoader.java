e377c92425fd1b57ea60489494c97f355a32115a
Arsen Baktiyarov <baktiyarov21@gmail.com>
2021-09-01 22:15:40 +0600

    Added Pets and Contact info ot owners on startup. Closes #25

--- pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/bootstrap/DataLoader.java
@@ -2,6 +2,7 @@ package com.arsenbaktiyarov.sfgpetclinic.bootstrap;
 
 
 import com.arsenbaktiyarov.sfgpetclinic.model.Owner;
+import com.arsenbaktiyarov.sfgpetclinic.model.Pet;
 import com.arsenbaktiyarov.sfgpetclinic.model.PetType;
 import com.arsenbaktiyarov.sfgpetclinic.model.Vet;
 import com.arsenbaktiyarov.sfgpetclinic.service.OwnerService;
@@ -9,6 +10,8 @@ import com.arsenbaktiyarov.sfgpetclinic.service.PetTypeService;
 import com.arsenbaktiyarov.sfgpetclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -42,18 +45,37 @@ public class DataLoader implements CommandLineRunner {
         owner.setId(1L);
         owner.setFirstName("Tony");
         owner.setLastName("Ferguson");
+        owner.setAddress("123 Ocean");
+        owner.setCity("Miami");
+        owner.setTelephone("100");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Sam");
+        owner.getPets().add(mikesPet);
         ownerService.save(owner);
+
         System.out.println("Owners loading....");
         Owner owner1 = new Owner();
         owner1.setId(2L);
         owner1.setFirstName("Colby");
         owner1.setLastName("Covington");
+        owner1.setAddress("123 Ocean");
+        owner1.setCity("Miami");
+        owner1.setTelephone("100");
         ownerService.save(owner1);
+
         Owner owner2 = new Owner();
         owner2.setId(3L);
         owner2.setFirstName("Robert");
         owner2.setLastName("Whittaker");
+        owner2.setAddress("123 Ocean");
+        owner2.setCity("Miami");
+        owner2.setTelephone("100");
         ownerService.save(owner2);
+
         System.out.println("Owners was created");
 
         Vet vet = new Vet();
