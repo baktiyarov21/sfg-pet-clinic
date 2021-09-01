64bfb09841f03034eff0e7124bb6f6cea6052140
Arsen Baktiyarov <baktiyarov21@gmail.com>
2021-09-01 22:23:57 +0600

    Added Specialties Map Service. Closes #23

---
+++ pet-clinic-data/src/main/java/com/arsenbaktiyarov/sfgpetclinic/service/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.arsenbaktiyarov.sfgpetclinic.service;
+
+import com.arsenbaktiyarov.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/arsenbaktiyarov/sfgpetclinic/service/map/SpecialtyMapService.java
@@ -0,0 +1,38 @@
+package com.arsenbaktiyarov.sfgpetclinic.service.map;
+
+import com.arsenbaktiyarov.sfgpetclinic.model.Specialty;
+import com.arsenbaktiyarov.sfgpetclinic.service.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+
+@Service
+public class SpecialtyMapService
+        extends AbstractMapService<Specialty, Long>
+        implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
