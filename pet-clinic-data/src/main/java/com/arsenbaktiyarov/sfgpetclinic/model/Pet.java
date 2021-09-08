7caba9efa6410b05c8c02a0a93b90c767168fbc7
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2021-09-08 11:09:03 +0600

    Updated Vet, Specialty with JPA mapping
    Implemented relationship between Pet and Visit entities
    
    Closes #30

--- pet-clinic-data/src/main/java/com/arsenbaktiyarov/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/arsenbaktiyarov/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.arsenbaktiyarov.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -17,6 +19,10 @@ public class Pet extends BaseEntity{
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
--- pet-clinic-data/src/main/java/com/arsenbaktiyarov/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/arsenbaktiyarov/sfgpetclinic/model/Visit.java
@@ -1,12 +1,22 @@
 package com.arsenbaktiyarov.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "descroption")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
+
 
 
 
