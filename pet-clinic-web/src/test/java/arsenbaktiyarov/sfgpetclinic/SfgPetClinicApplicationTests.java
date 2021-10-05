9a0fa45441a68403af08b72fa3c5392d8e67a89a
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2021-10-05 22:16:01 +0600

    Upgrade to JUnit5
    Upgraded pet-clinic-data's and pet-clinic-web's pom.xml:
     -Included maven-surefire-plugin
    
    Closes #37

--- pet-clinic-web/src/test/java/arsenbaktiyarov/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/arsenbaktiyarov/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -2,13 +2,15 @@ package arsenbaktiyarov.sfgpetclinic;
 
 import com.arsenbaktiyarov.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = SfgPetClinicApplication.class)
 class SfgPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
