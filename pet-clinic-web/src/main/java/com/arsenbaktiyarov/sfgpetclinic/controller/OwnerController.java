ff8e1dfb331b93505d5c9f8d7240b091a01009da
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2021-11-05 21:31:51 +0600

    Display owners record

--- pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/controller/OwnerController.java
@@ -4,7 +4,9 @@ import com.arsenbaktiyarov.sfgpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -24,9 +26,16 @@ public class OwnerController {
         return "owners/owners-list";
     }
 
-    @GetMapping({"/find", "/oups"})
+    @GetMapping({"/find", "/ops"})
     public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView modelAndView = new ModelAndView("owners/ownerDetails");
+        modelAndView.addObject(ownerService.findById(ownerId));
+        return modelAndView;
     }
 
 
