fd46344341217cbb23b4e4fba2b286ba1f450434
Arsen Baktiyarov <devbaktiyarov@gmail.com>
2021-11-18 20:51:08 +0600

    Add JSON endpoint on VetController
    Closes #57

--- pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/arsenbaktiyarov/sfgpetclinic/controller/VetController.java
@@ -1,10 +1,14 @@
 package com.arsenbaktiyarov.sfgpetclinic.controller;
 
+import com.arsenbaktiyarov.sfgpetclinic.model.Vet;
 import com.arsenbaktiyarov.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @RequestMapping("/vets")
 @Controller
@@ -17,10 +21,15 @@ public class VetController {
     }
 
 
-    @GetMapping({"","/", "/list",
+    @GetMapping({"", "/", "/list",
             "/vets-list", "/vets-list.html", "/vets.html"})
     public String listVets(Model model) {
-        model.addAttribute("vets",vetService.findAll());
+        model.addAttribute("vets", vetService.findAll());
         return "vets/vets-list";
+    }
+
+    @GetMapping("/api")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
